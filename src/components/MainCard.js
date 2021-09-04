95f6b9218daa011fb6cdca4e697bbdd88c9ac0e3
Leon <leonwheeler08@gmail.com>
2021-09-04 14:54:11 +0100

    Added avatar and styling for username and date on card.

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -1,8 +1,23 @@
 const MainCard = () => {
   return (
-    <div className="mainCard">
-      <div>
-        <img src="assets/images/adamshoe.png" className="cardImage" />
+    <div className="cardContainer">
+      <div className="mainCard">
+        <div>
+          <img src="assets/images/adamshoe.png" className="cardImage" />
+        </div>
+        <div className="cardDetails">
+          <div className="cardUsername">
+            <h3>@flewadam</h3>
+            <img
+              src="https://yt3.ggpht.com/Z_Ky2cO1dodI88N0ofZvBs1Rw8iAVW58jWWzMQOZbV75dfz1MZZmxfSw4SF5H7gk0ynCjdJc_YQ=s108-c-k-c0x00ffffff-no-rj"
+              alt="Avatar"
+              class="avatar"
+            />
+          </div>
+          <div className="cardDate">
+            <h3>03/09/2021</h3>
+          </div>
+        </div>
       </div>
     </div>
   );
